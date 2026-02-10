cdfab149f420ec76e9611c3fb55311d9d34b3dd0
Dhiraj M <dhirajmedipalli7@gmail.com>
2026-02-10 22:19:26 +0530

    Time: 0 ms (100%), Space: 19.5 MB (19.48%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.py
@@ -0,0 +1,13 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        n = len(matrix)
+        A = matrix
+
+        for i in range(n):
+            for j in range(i+1, n):
+                A[i][j], A[j][i] = A[j][i], A[i][j]
+
+        for i in range(n):
+            for j in range(n // 2):
+                A[i][j], A[i][n-j-1] = A[i][n-j-1], A[i][j]
+                
